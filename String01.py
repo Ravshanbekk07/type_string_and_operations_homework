a447c43ffbdfd5568f4ce16d2df14c48ecd15bb9
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-03 14:33:30 +0500

    alright

--- String01.py
+++ String01.py
@@ -1,8 +1,12 @@
-def main():
+def main(a):
     """"Return an optional string of length eight
     Args:
         None
     Returns:
         str: return answer.
     """
+   # c = '1234567890'
+    print(a[0:8])
     return 
+v = main('aaaazzzzpppp')
+print(v)
